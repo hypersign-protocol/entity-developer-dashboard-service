932e0be6d48d3644204d31189744b14c34559d82
varsha766 <varshakumari370@gmail.com>
2025-04-30 12:19:51 +0530

    small change

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ const hidNetworkUrls = Object.freeze({
   },
 });
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { logger: false });
+  const app = await NestFactory.create(AppModule);
 
   app.use(json({ limit: '10mb' }));
   app.use(urlencoded({ extended: true, limit: '10mb' }));
